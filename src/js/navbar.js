600c16a7a345603379ee306078334fddaa33d6d1
Rizalramzi <rizalramzi0@gmail.com>
2025-01-12 19:40:38 +0700

    first commit

---
+++ src/js/navbar.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll', function () {
+    const navbar = document.getElementById('navbar');
+    const logo = document.getElementById('logo')
+    
+    if (window.scrollY > 0) {
+      navbar.classList.add('bg-white', 'text-dark');
+      logo.src = 'assets/logo-dark.png'
+    } else {
+      navbar.classList.remove('bg-white', 'text-dark');
+      logo.src = 'assets/logo.png'
+    }
+  });
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        main : '#0E2E2E',
+        dark : '#323232',
+        gray : '#F2F2F2'
+      },
+    },
+  },
+  plugins: [],
+}
+
