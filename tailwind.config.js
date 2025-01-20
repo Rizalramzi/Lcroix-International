30f5f988d617fa849bc4eec75310470691d8e792
Rizalramzi <rizalramzi0@gmail.com>
2025-01-21 06:50:41 +0700

    Prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,12 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      colors:{
-        main : '#0E2E2E',
-        dark : '#323232',
-        gray : '#F2F2F2'
+      colors: {
+        main: "#0E2E2E",
+        dark: "#323232",
+        gray: "#F2F2F2",
       },
     },
   },
   plugins: [],
-}
-
+};
