ebcc1ba5ecf87667f42f42e8027fcfc5db8f2c2e
Muhammad Rizal Ramzi <126220782+Rizalramzi@users.noreply.github.com>
2025-02-02 09:27:57 +0700

    Update title

--- resources/views/collaboration.blade.php
+++ resources/views/collaboration.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Our Services</title>
+    <title>Collaboration | Lcroix International</title>
     <link rel="stylesheet" href="{{ asset("/css/style.css") }}" />
     <link rel="stylesheet" href="{{ asset("/css/font.css") }}" />
     @vite('resources/css/app.css')
